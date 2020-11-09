9a7819602e33067a0f630405c103bfadca377657
tyagi917 <tyagipranav000@gmail.com>
2020-11-09 22:39:52 +0530

    first commmit

---
+++ data.js
@@ -0,0 +1,84 @@
+const quizData = [
+  {
+    id: 0,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText:
+      "Who is the captain of Indian Cricket Team across all formats?",
+    questionImage: "",
+    options: [
+      "Rohit Sharma",
+      "Virat Kohli",
+      "Jasprit Bumrah",
+      "Cheteshwar Pujara"
+    ],
+    answer: "Virat Kohli"
+  },
+  {
+    id: 1,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: "What is the second highest mountain peak in India?",
+    questionImage: "",
+    options: ["Nanda Devi", "Kangchenjunga", "Karakoram", "Kamet"],
+    answer: "Nanda Devi"
+  },
+  {
+    id: 2,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: "India won its first Olympic hockey gold in which year?",
+    questionImage: "",
+    options: ["1928", "1932", "1936", "1948"],
+    answer: "1928"
+  },
+  {
+    id: 3,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: "The present Lok Sabha is the ____",
+    questionImage: "",
+    options: [
+      "14th Lok Sabha",
+      "15th Lok Sabha",
+      "16th Lok Sabha",
+      "17th Lok Sabha"
+    ],
+    answer: "17th Lok Sabha"
+  },
+  {
+    id: 4,
+    level: "Easy",
+    category: "Honour And Awards",
+    questionType: "text",
+    questionText: "	Which is the highest gallantry award in India?",
+    questionImage: "",
+    options: [
+      "Param Vishishtat Seva Medal",
+      "Param Vir Chakra",
+      "Kirti Chakra",
+      "Vir Chakra"
+    ],
+    answer: "Param Vir Chakra"
+  },
+  {
+    id: 5,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText:
+      "Who was the first Indian Chief of Army Staff of the Indian Army ?",
+    questionImage: "",
+    options: [
+      `Gen. K.M. Cariappa`,
+      `Vice-Admiral R.D. Katari`,
+      `Gen. Maharaja Rajendra Singhji`,
+      `None of the above`
+    ],
+    answer: `Gen. K.M. Cariappa`
+  }
+];
---
+++ gkdata.js
@@ -0,0 +1,84 @@
+const quizData = [
+  {
+    id: 0,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText:
+      "What is the full name of Dr. Abdul Kalam?",
+    questionImage: "",
+    options: [
+      "Avul Jakir Jalaluddin Kalam",
+      "Avul Pakir Jainulabdeen Abdul Kalam",
+      "Abdul Sakir Jainulabdeen Kalam",
+      "None of the above"
+    ],
+    answer: "Avul Pakir Jainulabdeen Abdul Kalam"
+  },
+  {
+    id: 1,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: "When was Dr. A.P.J. Abdul Kalam born?",
+    questionImage: "",
+    options: ["15 October 1931", "2 September 1929", "15 August 1923", " 29 February 1936"],
+    answer: "15 October 1931"
+  },
+  {
+    id: 2,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: ". Which island is named after Dr. A.P.J. Abdul Kalam?",
+    questionImage: "",
+    options: ["Wheeler Island, Odisha", "Landfall Island", "Bhavani Island", " Sriharikota"],
+    answer: "Wheeler Island, Odisha"
+  },
+  {
+    id: 3,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText: "The present Lok Sabha is the ____",
+    questionImage: "",
+    options: [
+      "14th Lok Sabha",
+      "15th Lok Sabha",
+      "16th Lok Sabha",
+      "17th Lok Sabha"
+    ],
+    answer: "17th Lok Sabha"
+  },
+  {
+    id: 4,
+    level: "Easy",
+    category: "Honour And Awards",
+    questionType: "text",
+    questionText: "  Which of the following award is not given to Dr. A.P.J. Abdul Kalam?",
+    questionImage: "",
+    options: [
+      " Padma Bhushan",
+      " Padma Vibhushan",
+      " Shanti Swaroop Bhatnagar",
+      "Bharat Ratna "
+    ],
+    answer: " Shanti Swaroop Bhatnagar"
+  },
+  {
+    id: 5,
+    level: "Easy",
+    category: "General Knowledge",
+    questionType: "text",
+    questionText:
+      "Who was the first Indian Chief of Army Staff of the Indian Army ?",
+    questionImage: "",
+    options: [
+      `Gen. K.M. Cariappa`,
+      `Vice-Admiral R.D. Katari`,
+      `Gen. Maharaja Rajendra Singhji`,
+      `None of the above`
+    ],
+    answer: `Gen. K.M. Cariappa`
+  }
+];
